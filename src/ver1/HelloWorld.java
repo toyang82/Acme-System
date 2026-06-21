deb2c9009c9ce159330b85963fd52b2e45e094c5
toyang82 <toyang@valdosta.edu>
2026-06-21 14:45:54 -0400

    added what for

--- src/ver1/HelloWorld.java
+++ src/ver1/HelloWorld.java
@@ -9,4 +9,10 @@ public class HelloWorld {
         String Msg = "Hey Now!";
         System.out.println(Msg);
     }
+
+    public static void whatFor() {
+        String Msg = "What For!";
+        System.out.println(Msg);
+    }
+
 }
